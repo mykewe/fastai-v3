ef43202ec434c18153b2e4dc29eb5a0add86741f
mykewe <61256058+mykewe@users.noreply.github.com>
2020-06-24 19:57:28 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -10,7 +10,7 @@ from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
 #export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/uc?export=download&id=1QVhVnqDiaKgro82g6qowczvzzJOy-pTt'
+export_file_url = 'https://www.dropbox.com/s/a7p4kh98n9kncb7/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 #classes = ['black', 'grizzly', 'teddys']
